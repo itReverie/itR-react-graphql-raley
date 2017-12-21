26965bd3ce9ba5ceba991b236c382357a0cc9a77
Brenda <brenda@Brendas-MacBook-Pro.local>
2017-12-21 17:06:53 -0500

    Adding the first schema for graphql

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import schema from './data/schema';
+import GraphQLHTTP from 'express-graphql';
 import { MongoClient} from 'mongodb';
 
 
@@ -8,7 +10,9 @@ let app = express();
 
 app.use(express.static('public'));
 
-
+app.use('/graphql', GraphQLHTTP({
+  schema
+}));
 
 //This is an option to keep the db obfuscated
 //console.log(process.env.MONGO_URL); //process.env.MONGO_URL,
